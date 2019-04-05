f009de45142c8109fff6120faf4a2d3e5f43a8f6
Mike Flores <mike.flores3717@gmail.com>
2019-04-05 15:26:08 -0500

    committing added stuff

---
+++ src/JavaTester.java
@@ -0,0 +1,10 @@
+public class JavaTester {
+    public static void main(String[] args) {
+        Student student = new Student("bob");
+        student.addGrade(90);
+        student.addGrade(95);
+        student.addGrade(100);
+
+        System.out.println(student.getGradeAverage());
+    }
+}
